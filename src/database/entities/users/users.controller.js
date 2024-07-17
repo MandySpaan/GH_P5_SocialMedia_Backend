03ba82ac7ff23f6173f7d41779ef93a673eb7e15
MandySpaan <mandyspaan94@gmail.com>
2024-07-17 18:44:49 +0200

    feat: getAllUsers

--- src/database/entities/users/users.controller.js
+++ src/database/entities/users/users.controller.js
@@ -1,2 +1,18 @@
-import bcrypt from "bcrypt";
 import User from "./user.model.js";
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find().select("-password");
+    res.status(200).json({
+      success: true,
+      message: "Users retrieved",
+      data: users,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Users can't be retrieved",
+      error: error.message,
+    });
+  }
+};
--- src/database/entities/users/users.routes.js
+++ src/database/entities/users/users.routes.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import { auth } from "../middlewares/auth.js";
+import { isSuperAdmin } from "../middlewares/isSuperAdmin.js";
+import { getAllUsers } from "./users.controller.js";
 
 const router = Router();
+
+router.get("/", auth, isSuperAdmin, getAllUsers);
 
 export { router };
