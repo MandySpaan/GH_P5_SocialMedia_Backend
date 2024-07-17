841994f2138eaa18ba7a873266fff33e8c96a6cc
MandySpaan <mandyspaan94@gmail.com>
2024-07-17 16:25:50 +0200

    feat: user model created

---
+++ src/database/entities/users/user.model.js
@@ -0,0 +1,43 @@
+import { Schema, model } from "mongoose";
+
+const UserShema = new Schema(
+  {
+    first_name: {
+      type: String,
+      required: true,
+    },
+
+    last_name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    is_active: {
+      type: Boolean,
+      default: true,
+    },
+
+    role: {
+      type: String,
+      Enum: ["user", "admin", "super_admin"],
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = model("User", UserShema);
+
+export default User;
