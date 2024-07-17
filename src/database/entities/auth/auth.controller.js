9b0e2b598508f307c6e55835fae93016d3e21cdb
MandySpaan <mandyspaan94@gmail.com>
2024-07-17 18:30:08 +0200

    feat: register and login

--- src/database/entities/auth/auth.controller.js
+++ src/database/entities/auth/auth.controller.js
@@ -1,9 +1,24 @@
 import bcrypt from "bcrypt";
 import User from "../users/user.model.js";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
   try {
     const { first_name, last_name, email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: " Email and password are required ",
+      });
+    }
+
+    if (password.length < 8 || password.length > 12) {
+      return res.status(400).json({
+        success: false,
+        message: "Password is not valid, 8 to 12 characters must be needed",
+      });
+    }
 
     const hashedPassword = bcrypt.hashSync(
       password,
@@ -27,6 +42,63 @@ export const register = async (req, res) => {
       success: false,
       message: " Error registering user",
       error: error.message,
+    });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        success: true,
+        message: "email and password are required",
+      });
+    }
+
+    const user = await User.findOne({
+      email: email,
+    });
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "email or password are invalid",
+      });
+    }
+
+    const passwordVerified = bcrypt.compareSync(password, user.password);
+    console.log(1);
+    if (!passwordVerified) {
+      return res.status(400).json({
+        success: false,
+        message: "email or password are invalid",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+        roles: user.role,
+      },
+
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "2h",
+      }
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "User logged",
+      data: token,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error loggin user",
+      error: error,
     });
   }
 };
--- src/router.js
+++ src/router.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { router as userRoutes } from "./database/entities/users/users.routes.js";
 import { router as authRoutes } from "./database/entities/auth/auth.routes.js";
+import { router as userRoutes } from "./database/entities/users/users.routes.js";
 
 const router = Router();
 
-router.use("/users", userRoutes);
 router.use("/auth", authRoutes);
+router.use("/users", userRoutes);
 
 console.log();
 
