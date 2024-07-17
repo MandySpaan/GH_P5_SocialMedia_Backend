47c92c5b0f81a7b57138e97a9f1e89ae5f267670
MandySpaan <mandyspaan94@gmail.com>
2024-07-17 18:34:01 +0200

    feat(middleware): isSuperAdmin

---
+++ src/database/entities/middlewares/isSuperAdmin.js
@@ -0,0 +1,18 @@
+export const isSuperAdmin = async (req, res, next) => {
+  try {
+    if (req.tokenData.role_id !== "super_admin") {
+      return res.json({
+        success: false,
+        message: "you are not allowed",
+      });
+    }
+
+    next();
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "ERROR",
+      error: error.message,
+    });
+  }
+};
